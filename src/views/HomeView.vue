2962d58c8953bb4b0b7a7520dfbaebcf758e3f75
morningClock <785674145@qq.com>
2022-08-16 18:09:13 +0800

    add lint-staged to lint commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,5 +14,21 @@ export default {
   components: {
     HelloWorld,
   },
+  methods: {
+    testFunction() {
+      // 我是单引号
+      const str1 = "hello World";
+      // 我是双引号
+      const str2 = "hello World";
+      // 我是无结束符
+      const str3 = "hello World";
+      // 我是有结束符
+      const str4 = "hello World";
+      console.log("sayHello", str1);
+      console.log("sayHello", str2);
+      console.log("sayHello", str3);
+      console.log("sayHello", str4);
+    },
+  },
 };
 </script>
